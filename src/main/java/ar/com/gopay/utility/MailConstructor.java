2c8292d0ca72694a85d224c13555f1eebe6482f2
luchoaglp <lucho_aglp@hotmail.com>
2019-06-10 14:55:35 -0300

    Update #13

--- src/main/java/ar/com/gopay/utility/MailConstructor.java
+++ src/main/java/ar/com/gopay/utility/MailConstructor.java
@@ -8,21 +8,28 @@ import org.springframework.stereotype.Component;
 import java.util.Locale;
 
 @Component
-public class MailConstructor {
+public class EmailServiceImpl implements EmailServi {
 
     private final Environment env;
 
-    public MailConstructor(Environment env) {
+    public EmailServiceImpl(Environment env) {
         this.env = env;
     }
-
 
     public SimpleMailMessage constructSignUpTokenEmail(String contextPath,
                                                        Locale locale,
                                                        String token,
                                                        RecoveryPasswordClient recoveryPasswordClient) {
 
+        SimpleMailMessage email = new SimpleMailMessage();
+
         String url = contextPath + "/account/recovery/edit/password/" + token;
+
+        email.setTo(recoveryPasswordClient.getEmail());
+        email.setSubject("GoPay - Recuperá tu clave");
+        email.setText(url);
+        //emailSender.send(message);
+        /*
         //String message = "\nPlease click on this link to verify your email and edit your personal information. ";
 
         SimpleMailMessage email = new SimpleMailMessage();
@@ -30,6 +37,7 @@ public class MailConstructor {
         email.setSubject("GoPay - Recuperá tu clave");
         email.setText(url);
         email.setFrom(env.getProperty("support.email"));
+        */
 
         return email;
     }
